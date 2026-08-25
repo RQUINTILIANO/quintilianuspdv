6711ec135492d935c7bfb69f27fb70eb004527c1
Rafaela Quintiliano <106849608+RQUINTILIANO@users.noreply.github.com>
2026-08-24 22:29:47 -0300

    Add files via upload

--- Quintilianuspdv/src/database/Database.java
+++ Quintilianuspdv/src/database/Database.java
@@ -1,95 +1,45 @@
-<<<<<<< HEAD
 package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-//informação de banco de dados
-//Driver está relacionado a biblioteca
 public class Database {
+	// informações do banco de dados
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/quintpdv";
 	private String user = "root";
 	private String password = "";
 	
-	//Método = função, para conectar um banco 
+	// método("função") para conectar o banco
 	public Connection conectar() {
-	
 		try {
 			Class.forName(driver);
-			return  DriverManager.getConnection(url, user, password);
-			//abre a conexão com o banco (Abrir geladeira) 
+			return DriverManager.getConnection(url, user, password); //abre a conexão com o banco
 		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 	}
-	//Método para testar a conexão (Verificar status de conexão)
+	
+	//método para testar a conexão (status de conexão)
 	public boolean testarConexao() {
-		//a linha abaixo cria o objeto con Que é responsábel por estabelecer a conexão com o banco 
+		// alinha abaixo cria o objeto con que é responsável por estabelecer a conexão com o banco
 		Connection con = conectar();
 		
-		//se não conseguir estabelecer a conexão // Esse comando abre a geladeira
+		//se não conseguir estabelecer a conexão
 		if (con == null) {
 			return false;
 		}
 		
+		// na tentativa de fechar a conexão
 		try {
-			con.close();  //encerrar a conexão após o teste
-			return true;
+			con.close(); //encerra a conexão após o teste
+			return true; //retorno verdadeiro para Main
 		} catch (Exception e) {
-			//se não conseguir fechar a conexão
-		System.out.println(e);
-		return false;
-		}
-		}
-
-}
-=======
-package database;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-//informação de banco de dados
-//Driver está relacionado a biblioteca
-public class Database {
-	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://localhost:3306/quintpdv";
-	private String user = "root";
-	private String password = "";
-	
-	//Método = função, para conectar um banco 
-	public Connection conectar() {
-	
-		try {
-			Class.forName(driver);
-			return  DriverManager.getConnection(url, user, password);
-			//abre a conexão com o banco (Abrir geladeira) 
-		} catch (Exception e) {
+			// se não conseguir fechar a conexão
 			System.out.println(e);
-			return null;
+			return false;
 		}
 	}
-	//Método para testar a conexão (Verificar status de conexão)
-	public boolean testarConexao() {
-		//a linha abaixo cria o objeto con Que é responsábel por estabelecer a conexão com o banco 
-		Connection con = conectar();
-		
-		//se não conseguir estabelecer a conexão // Esse comando abre a geladeira
-		if (con == null) {
-			return false;
-		}
-		
-		try {
-			con.close();  //encerrar a conexão após o teste
-			return true;
-		} catch (Exception e) {
-			//se não conseguir fechar a conexão
-		System.out.println(e);
-		return false;
-		}
-		}
 
 }
->>>>>>> 0c2cd6bf67a41c2a09387f29e30829f87f53c182
--- Quintilianuspdv/src/view/frmFornecedor.java
+++ Quintilianuspdv/src/view/frmFornecedor.java
@@ -14,10 +14,10 @@ public class frmFornecedor extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtNome;
+	private JTextField txtFone;
+	private JTextField txtEmail;
+	private JTextField txtID;
 
 	/**
 	 * Launch the application.
@@ -56,50 +56,52 @@ public class frmFornecedor extends JDialog {
 			lblNewLabel_2.setBounds(49, 300, 46, 14);
 			contentPanel.add(lblNewLabel_2);
 		}
+
+		txtNome = new JTextField();
+		txtNome.setBounds(49, 121, 384, 20);
+		contentPanel.add(txtNome);
+		txtNome.setColumns(10);
+
+		txtFone = new JTextField();
+		txtFone.setBounds(49, 216, 231, 20);
+		contentPanel.add(txtFone);
+		txtFone.setColumns(10);
+
+		txtEmail = new JTextField();
+		txtEmail.setBounds(49, 317, 384, 20);
+		contentPanel.add(txtEmail);
+		txtEmail.setColumns(10);
+
+		JButton btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.setBounds(49, 362, 89, 23);
+		contentPanel.add(btnAdicionar);
+
+		JButton btnEditar = new JButton("Editar");
 		
-		textField = new JTextField();
-		textField.setBounds(49, 121, 384, 20);
-		contentPanel.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(49, 216, 231, 20);
-		contentPanel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(49, 317, 384, 20);
-		contentPanel.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Adicionar");
-		btnNewButton.setBounds(49, 362, 89, 23);
-		contentPanel.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Editar");
-		btnNewButton_1.setBounds(188, 362, 89, 23);
-		contentPanel.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Excluir");
-		btnNewButton_2.setBounds(338, 362, 89, 23);
-		contentPanel.add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("Todos");
-		btnNewButton_3.setBounds(493, 362, 89, 23);
-		contentPanel.add(btnNewButton_3);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(49, 58, 255, 20);
-		contentPanel.add(textField_3);
-		textField_3.setColumns(10);
-		
+		//==============CRUD Update -  Editar Fornecedor======================
+		btnEditar.setBounds(188, 362, 89, 23);
+		contentPanel.add(btnEditar);
+
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(338, 362, 89, 23);
+		contentPanel.add(btnExcluir);
+
+		JButton btnTodos = new JButton("Todos");
+		btnTodos.setBounds(493, 362, 89, 23);
+		contentPanel.add(btnTodos);
+
+		txtID = new JTextField();
+		txtID.setBounds(49, 58, 255, 20);
+		contentPanel.add(txtID);
+		txtID.setColumns(10);
+
 		JLabel lblId = new JLabel("ID:");
 		lblId.setBounds(49, 33, 46, 14);
 		contentPanel.add(lblId);
-		
-		JButton btnNewButton_4 = new JButton("Buscar");
-		btnNewButton_4.setBounds(446, 120, 89, 23);
-		contentPanel.add(btnNewButton_4);
+
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(446, 120, 89, 23);
+		contentPanel.add(btnBuscar);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -116,18 +118,15 @@ public class frmFornecedor extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
-	} //fim construtor
-	
-	//LIMPAR CAMPOS
+	} // fim construtor
+
+	// LIMPAR CAMPOS
 	void limparCampos() {
 		txtID.setText(null);
 		txtNome.setText(null);
 		txtFone.setText(null);
 		txtEmail.setText(null);
-		txtIDNome.requestFocus();
+		txtNome.requestFocus();
 	}
 
-	
 }// fim do código
-
-
