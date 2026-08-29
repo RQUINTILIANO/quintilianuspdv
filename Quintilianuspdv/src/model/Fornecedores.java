66a0ff97781c6474cc8c60fe9305e160760a2e51
RQuintiliano <rafaela.quintilianoo@outlook.com>
2026-08-28 21:23:38 -0300

    28082026

---
+++ Quintilianuspdv/src/model/Fornecedores.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Fornecedores {
+	// Modelo de dados da tabela fornecedor
+	private int idFornecedor;
+	private String nome;
+	private String fone;
+	private String email;
+	public int getIdFornecedor() {
+		return idFornecedor;
+	}
+	public void setIdFornecedor(int idFornecedor) {
+		this.idFornecedor = idFornecedor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+//gerar getters e setter de forma automática
+//botão direito
+
+}
